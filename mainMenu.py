f0f675cd4a4bd0d4acb4251f37fc8c0c7af3f990
miguel-peppe <pedropeps2828@gmail.com>
2023-10-16 16:05:02 -0300

    att

--- mainMenu.py
+++ mainMenu.py
@@ -1,4 +1,15 @@
 import os
+import sys
+import time
+
+def escrita(texto):
+    for letra in texto:
+        sys.stdout.write(letra)
+        sys.stdout.flush()
+        time.sleep(0.09)
+
+'''texto = 'Welcome to ancient History'
+escrita(texto)'''
 
 class GameMenu:
     def __init__(self):
@@ -97,8 +108,8 @@ class GameMenu:
 
     def warrior_character(self):
         print("                     .-'`-.")
-        print("                    / | |  \ ")
-        print("                   /  | |   \ ")
+        print("                    / | |   \ ")
+        print("                   /  | |    \ ")
         print("                  |___|_|__  |")
         print("                  ||<o>| <o>`|")
         print("                  ||   J_   )|")
@@ -136,6 +147,10 @@ class GameMenu:
 
     def main_name(self):
         print('            Ancient History            ')
+
+    def main_name_init(self):
+        texto = '       Welcome to ancient History \n'
+        escrita(texto)
 
     def options(self):
         print('    > Play     [P]')
@@ -180,21 +195,21 @@ class GameMenu:
         self.barra2()
 
         help_text = input("""
-        Welcome to Ancient History Help!
+            Welcome to Ancient History Help!
 
-        This is a simple text-based game. In this game, you can choose to:
+            This is a simple text-based game. In this game, you can choose to:
 
-        1. Play the game and go on an adventure.
-        2. Access the Help menu to learn more about the game.
-        3. Quit the game.
+            1. Play the game and go on an adventure.
+            2. Access the Help menu to learn more about the game.
+            3. Quit the game.
 
-        To play the game, select "Play" by entering 'P' in the menu.
-        To access this Help menu, select "Help" by entering 'H' in the menu.
-        To quit the game, select "Quit" by entering 'Q' in the menu.
+            To play the game, select "Play" by entering 'P' in the menu.
+            To access this Help menu, select "Help" by entering 'H' in the menu.
+            To quit the game, select "Quit" by entering 'Q' in the menu.
 
-        Have fun and enjoy your journey through Ancient History!
+            Have fun and enjoy your journey through Ancient History!
 
-        Press Enter to return to the main menu...
+            Press Enter to return to the main menu...
         """)
         if help_text == '':
             return self.play()
@@ -206,7 +221,7 @@ class GameMenu:
         self.clear_screen()
         self.main_logo()
         self.barra()
-        self.main_name()
+        self.main_name_init()
         self.barra2()
         self.options()
         
@@ -234,18 +249,81 @@ class GameMenu:
         if character_select == 'm':
             self.clear_screen()
             self.mage()
+            self.init_name("mage")
         elif character_select == 'a':
             self.clear_screen()
             self.assassin()
+            self.init_name("assassin")
         elif character_select == 'w':
             self.clear_screen()
             self.warrior()
+            self.init_name("warrior")
         elif character_select == 'b':
             self.play()
         else:
-            print('Invalid option')
             self.clear_screen()
             self.start()
+
+    def init_name(self, character_name):
+        self.clear_screen()
+        print(f'    Congratulations, you decided to play with {character_name}')
+        self.barra()
+        name = input("    Your name will be: ")
+        self.barra2()
+
+        if character_name == "mage":
+            self.mage_story(name)
+        elif character_name == "assassin":
+            self.assassin_story(name)
+        elif character_name == "warrior":
+            self.warrior_story(name)
+
+    def mage_story(self, name):
+        self.clear_screen()
+        self.barra()
+        print(f'    Welcome, {name} the Mage!')
+        self.barra2()
+        input("Press Enter to continue...")
+
+        self.clear_screen()
+        self.barra()
+        print("    You are a powerful Mage on a quest to save the kingdom from the evil dragon.")
+        print("    Your magic abilities will be your greatest asset.")
+        self.barra2()
+        input("Press Enter to continue...")
+
+        # Adicione mais detalhes à história do Mage aqui...
+
+    def assassin_story(self, name):
+        self.clear_screen()
+        self.barra()
+        print(f'    Welcome, {name} the Assassin!')
+        self.barra2()
+        input("Press Enter to continue...")
+
+        self.clear_screen()
+        self.barra()
+        print("    You are a stealthy Assassin tasked with eliminating the kingdom's enemies.")
+        print("    Your agility and precision will be your keys to success.")
+        self.barra2()
+        input("Press Enter to continue...")
+
+        # Adicione mais detalhes à história do Assassino aqui...
+
+    def warrior_story(self, name):
+        self.clear_screen()
+        self.barra()
+        print(f'    Welcome, {name} the Warrior!')
+        self.barra2()
+        input("Press Enter to continue...")
+
+        self.clear_screen()
+        self.barra()
+        print("    You are a fearless Warrior chosen to defend the kingdom against its foes.")
+        print("    Your strength and bravery will be your greatest weapons.")
+        self.barra2()
+        input(" Press Enter to continue...")
+
 
 menu = GameMenu()
 menu.play()
